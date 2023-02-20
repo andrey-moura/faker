d78e4e3fe2437db52be6797720bde7c8f3f4db26
Andrey Moura <andreymds88@gmail.com>
2023-02-19 23:02:35 -0300

    Remove faker::sequence; add faker::array::ascending and faker::array::random

--- faker.hpp
+++ faker.hpp
@@ -33,20 +33,6 @@ namespace uva
             double real();
         };
 
-        namespace sequence
-        {
-            template<typename T>
-            std::vector<T> from_to(const T& from, const T& to)
-            {
-                std::vector<T> seq;
-                for(T index = from; index < to; ++index)
-                {
-                    seq.push_back(index);
-                }
-                return seq;
-            }
-        };
-
         namespace commerce
         {
             std::string product();
@@ -60,6 +46,49 @@ namespace uva
             std::string sentence_end();
             std::string sentence(int min_words = 4, int max_words = 10);
             std::string paragraph(int setences = 1);
+        };
+
+        namespace array
+        {
+            /// @brief Generates an array with random values
+            /// @tparam T value type
+            /// @param n number of elements
+            /// @return A std::vector<T> with n elements
+            template<typename T>
+            std::vector<T> random(int n)
+            {
+                std::vector<T> array;
+                array.reserve(n);
+
+                for(size_t i = 0; i < n; ++i)
+                {
+                    if constexpr(std::is_same<T,int>::value)
+                    {
+                        array.push_back(number::integer());
+                    } else if (std::is_same<T,double>::value || std::is_same<T,float>::value) {
+                        array.push_back(number::real());
+                    } else {
+                        throw std::exception(std::format("random is not implemented for {}", typeid(T).name()));
+                    }
+                }
+
+                return array;
+            }
+            /// @brief Generates an array with ascending values
+            /// @tparam T value type
+            /// @param from The first value
+            /// @param to The last value
+            /// @return A std::vector<T> with from, ..., to
+            template<typename T>
+            std::vector<T> ascending(const T& from, const T& to)
+            {
+                std::vector<T> seq;
+                for(T index = from; index != to; ++index)
+                {
+                    seq.push_back(index);
+                }
+                return seq;
+            }
         };
     };
 };
